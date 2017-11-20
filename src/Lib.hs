f36ab31a6bfd8241b082c8bf45a388b4c9a88cc7
Adam Washington <adam.washington@stfc.ac.uk>
2017-11-20 16:30:05 +0000

    Unified Status Bar

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,47 @@
+{-# LANGUAGE Rank2Types #-}
 module Lib
     ( someFunc
     ) where
 
 import Control.Concurrent (threadDelay, forkIO)
+import Control.Lens as L
 import Control.Monad (forever)
+import Data.Time.Calendar
+import Data.Time.Format
 import Data.Time.LocalTime
 import Pipes as P
 import Pipes.Concurrent as P
 import qualified Pipes.Prelude as P
 import System.Process
+
+data Bar = Bar {
+  _clock :: ZonedTime,
+  _disks :: Double,
+  _mail :: Int,
+  _cpu :: Double,
+  _mem :: Double}
+
+defaultBar = Bar {
+  _clock = ZonedTime (LocalTime (ModifiedJulianDay 0) midnight) utc,
+  _disks = 0,
+  _mail = 0,
+  _cpu = 0,
+  _mem = 0}
+
+instance Show Bar where
+  show x = (formatTime defaultTimeLocale "%R" $ _clock x) ++ " " ++ toBar (_disks x) ++ " " ++
+    show (_mail x) ++ " " ++ toBar (_cpu x) ++ " " ++ toBar (_mem x)
+
+__clock :: Lens' Bar ZonedTime
+__clock = lens _clock (\ bar x -> bar{_clock=x})
+__disks :: Lens' Bar Double
+__disks = lens _disks (\ bar x -> bar{_disks=x})
+__mail :: Lens' Bar Int
+__mail = lens _mail (\ bar x -> bar{_mail=x})
+__cpu :: Lens' Bar Double
+__cpu = lens _cpu (\ bar x -> bar{_cpu=x})
+__mem :: Lens' Bar Double
+__mem = lens _mem (\ bar x -> bar{_mem=x})
 
 clockTime ::Producer ZonedTime IO r
 clockTime = forever $ do
@@ -36,8 +69,8 @@ cpu = forever $ do
 mem :: Producer Double IO r
 mem = forever $ do
   result <- lift $ readFile "/proc/meminfo"
-  let tot = read . head . drop 1 . words . head . lines $ result
-      free = read . head . drop 1 . words . head . drop 1 . lines $ result
+  let tot = read . (!! 1) . words . head . lines $ result
+      free = read . (!! 1) . words . (!! 1) . lines $ result
   yield $ (tot-free)/tot
   lift $ threadDelay 5000000
 
@@ -50,31 +83,31 @@ unique state = do
     unique new
     else unique new
 
-watchProducer :: (Show a) => Producer a IO () -> Output String -> IO ()
-watchProducer p output = do
+apply :: (Monad m) => x -> Pipe (x -> x) x m r
+apply x = do
+  result <- await
+  let new = result x
+  yield new
+  apply new
+
+watchProducer :: (Show a) => Producer a IO () -> Lens' Bar a -> Output (Bar -> Bar) -> IO ()
+watchProducer p lens output = do
   _ <- forkIO $ do
-    runEffect $ p >-> P.show >-> unique "" >-> toOutput output
+    runEffect $ p >-> P.map (\x bar -> bar & lens .~ x) >-> toOutput output
     performGC
   return ()
 
 toBar :: Double -> String
 toBar x = "^r(3x" ++ show (round (x*10)) ++ ")"
 
-watchBar :: Producer Double IO () -> Output String -> IO ()
-watchBar p output = do
-  _ <- forkIO $ do
-    runEffect $ p >-> unique 0 >-> P.map toBar >-> toOutput output
-    performGC
-  return ()
-
 someFunc :: IO ()
 someFunc = do
   (output, input) <- spawn unbounded
 
-  watchProducer clockTime output
-  watchBar disks output
-  watchProducer mail output
-  watchBar cpu output
-  watchBar mem output
+  watchProducer clockTime __clock output
+  watchProducer disks __disks output
+  watchProducer mail __mail output
+  watchProducer cpu __cpu output
+  watchProducer mem __mem output
 
-  runEffect $ fromInput input >-> P.stdoutLn
+  runEffect $ fromInput input >-> apply defaultBar >-> P.show >-> unique "" >-> P.stdoutLn
